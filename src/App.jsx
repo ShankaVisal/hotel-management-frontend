439f6b420e37307e52d580a5525ca0a5f07b2f9a
Shanka Visal <shankavisal@gmail.com>
2024-12-05 23:24:58 +0530

    update tailwind csse

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,18 @@ function App() {
   return (
     <>
       <Header/>
+      <div className="w-full h-screen bg-blue-900 flex">
+        <div className="border border-white bg-white h-[100px] w-[700px] rounded-lg flex items-center justify-center">
+          <input type="date"/>
+          <input type="date"/>
+          <select>
+            <option>Luxury</option>
+            <option>Normal</option>
+          </select>
+          <button>Book Now</button>
+        </div>
+
+      </div>
     </>
   )
 }
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,11 +1,11 @@
 import UserTag from "../userData/userdata.jsx";
-import "./header.css"
+
 
 function Header(){
     return(
-        <header>
-            <h1 className="text-red-500 text-[30px]">Hotel Management System</h1>
-            <UserTag imageLink = "https://cdn.pixabay.com/photo/2015/03/04/22/35/avatar-659652_640.png" name ="Testing"/>
+        <header className="w-full bg-blue-500 flex h-[100px] relative items-center p-[10px]">
+            <h1 className="text-white text-[30px]">Leonine villa</h1>
+            <UserTag imageLink = "https://cdn.pixabay.com/photo/2015/03/04/22/35/avatar-659652_640.png" name ="Shanka Visal"/>
         </header>
     )
 }
