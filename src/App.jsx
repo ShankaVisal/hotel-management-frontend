0dbced2973d25a2f835b74342bf0092a2a66115f
Shanka Visal <shankavisal@gmail.com>
2024-12-11 23:52:42 +0530

    add admin pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./app.css";
-import AdminPage from "./pages/admin page/admin";
-import Homepage from "./pages/client page/Homepage";
+import AdminPage from "./pages/admin/admin page/admin.jsx";
+import Homepage from "./pages/client page/homepage.jsx";
 
 function App() {
   return (
---
+++ src/pages/admin/bookings/adminBooking.jsx
@@ -0,0 +1,9 @@
+export default function AdminBoking(){
+    return(
+        <>
+            <div className="">
+
+            </div>
+        </>
+    )
+}
